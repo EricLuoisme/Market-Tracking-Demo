60a0dee8f8d54f7bd2f925e8dbb13eae158b876c
roylic <lykismekk@outlook.com>
2023-07-01 09:54:08 +0800

    current polygon.io upgrade to v2 but the legendary go-sdk still using v1

--- polygon-io-demo/polygon_api_test.go
+++ polygon-io-demo/polygon_api_test.go
@@ -5,27 +5,45 @@ import (
 	polygon "github.com/polygon-io/client-go/rest"
 	"github.com/polygon-io/client-go/rest/models"
 	"log"
-	"strconv"
 	"testing"
 	"time"
 )
 
+var (
+	apiKey   = "LtsCZadfcYZn3qtiOQ8_3oDpnL7NJ3rQ"
+	adjusted = true
+	cli      = polygon.New(apiKey)
+)
+
+// Test_StockDaily is for daily stock market place info
 func Test_StockDaily(t *testing.T) {
 
-	cli := polygon.New("LtsCZadfcYZn3qtiOQ8_3oDpnL7NJ3rQ")
-
-	params := &models.GetGroupedDailyAggsParams{
+	params := models.GetGroupedDailyAggsParams{
 		Locale:     models.US,
 		MarketType: models.Stocks,
 		Date:       models.Date(time.Date(2023, 6, 29, 0, 0, 0, 0, time.UTC)),
-	}
+	}.WithAdjusted(true)
 
-	resp, err := cli.GetGroupedDailyAggs(context.Background(), params,
-		models.APIKey("LtsCZadfcYZn3qtiOQ8_3oDpnL7NJ3rQ"),
-		models.QueryParam("adjusted", strconv.FormatBool(true)))
+	resp, err := cli.GetGroupedDailyAggs(context.Background(), params)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	log.Println(resp)
+}
+
+// Test_OpenCloseDaily is for daily open & close position
+func Test_OpenCloseDaily(t *testing.T) {
+
+	params := models.GetDailyOpenCloseAggParams{
+		Ticker: "APPL",
+		Date:   models.Date(time.Date(2020, 10, 14, 0, 0, 0, 0, time.Local)),
+	}.WithAdjusted(true)
+
+	agg, err := cli.GetDailyOpenCloseAgg(context.Background(), params)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(agg)
 }
